a9b21f03d2c631082afa65e4fd053facc99c05f3
Joseph Hsieh <webjoe@gmail.com>
2012-04-20 16:39:34 -0700

    Genearl Settings File for GeekTool

---
+++ settings.php
@@ -0,0 +1,12 @@
+<?php
+// Configurations for GeekTool
+
+// Chartbeat API Keys
+//    Host => API Key
+
+$chartbeat = array(
+	"foo.com" => "abcd1234abcd1234abcd1234abcd1234",
+	"bar.com" => "abcd1234abcd1234abcd1234abcd1234",
+);
+
+?>

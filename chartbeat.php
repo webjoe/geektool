3c1fa668efebac11b469d19d78c44b42112d2d85
Joseph Hsieh <webjoe@gmail.com>
2012-04-21 15:45:23 -0700

    Updated chartbeat.php to take CLI arguments
    -h host -p path

--- chartbeat.php
+++ chartbeat.php
@@ -1,19 +1,38 @@
 <?php
 include("settings.php");
 
-// http://api.chartbeat.com/live/quickstats/?host=[host]&path=[path]&apikey=[apikey]
+# var_dump($argv); // Verifying CLI Arguments
 
-$host = "foo.com";
+// Get the Host Name from CLI
+if(array_search('-h', $argv))
+{
+	(integer)$key = array_search('-h', $argv);
+	$host = $argv[$key+1];
+}
+else
+{
+	echo "No host specified.  Use chartbeat.php -h [host]\n";
+}
+
+//Get the Path form CLI
+if(array_search('-p', $argv))
+{
+	(integer)$key = array_search('-p', $argv);
+	$pathx = $argv[$key+1];
+}
+
 $apikey = $chartbeat[$host];
 $path = "";
 
-$rest = "http://api.chartbeat.com/live/quickstats/?host=$host&path=$path&apikey=$apikey";
+// http://api.chartbeat.com/live/quickstats/?host=[host]&path=[path]&apikey=[apikey]
+// REST API Docs: http://chartbeat.pbworks.com/w/page/29751979/quickstats
+$chartbeat_rest_url = "http://api.chartbeat.com/live/quickstats/?host=$host&path=$path&apikey=$apikey";
 
 // create a new cURL resource
 $ch = curl_init();
 
 // set URL and other appropriate options
-curl_setopt($ch, CURLOPT_URL, $rest);
+curl_setopt($ch, CURLOPT_URL, $chartbeat_rest_url);
 curl_setopt($ch, CURLOPT_HEADER, 0);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
@@ -23,8 +42,8 @@ $response = curl_exec($ch);
 // close cURL resource, and free up system resources
 curl_close($ch);
 
-$jsonObject = json_decode($response);
+$jsonO = json_decode($response);
 
-echo $jsonObject->{'people'};
+echo $jsonO->{'people'};
 
 ?>
